7f0356c4ffac271a84f447974e2f9721f0240da8
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2024-01-27 12:22:57 -0500

    comment model impplemented

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,9 +1,21 @@
 const mongoose =require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-    author: mongoose.Schema.Types.ObjectId, 
-    post: mongoose.Schema.Types.ObjectId, 
-    content: String,
+
+    author: 
+    {type:mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }, 
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      required: true
+    }, 
+    content: {
+      type: String,
+      required: true
+    },
    
   },{timestamps:true
   });
